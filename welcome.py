2bbb8347ef6b65ae074c07e1df18afba1b761846
HAMID <abdulhamidabari652@gmail.com>
2024-10-17 04:25:45 +0100

    welcome

--- welcome.py
+++ welcome.py
@@ -1,1 +1,9 @@
 print("hello dynamite, welcome to programming class with python")
+
+first_name="ABARI"
+last_name="HAMID"
+e_mail="abdulhamidabari652@gmail.com"
+
+print(f"your first name is= {first_name}")
+print(f"your last name= {last_name}")
+print(f"your email is= {e_mail}")
