f26e5a7f746654291dcfd71fd95c32a3a30fa8dc
HAMID <abdulhamidabari652@gmail.com>
2024-10-19 08:03:45 +0100

    square

---
+++ Square.py
@@ -0,0 +1,25 @@
+num6=0
+num1=1
+num2=2
+num3=3
+num4=4
+num5=5
+print("number            square         cube")
+sum1 = num6**2
+sum2 = num6**3
+print(f"{num6}                     {sum1}                {sum2}")
+sum3 = num1**2
+sum4 = num1**3
+print(f"{num1}                     {sum3}                {sum4}")
+sum5 = num2**2
+sum6 = num2**3
+print(f"{num2}                     {sum5}                {sum6}")
+sum7 = num3**2
+sum8 = num3**3
+print(f"{num3}                     {sum7}                {sum8}")
+sum9 = num4**2
+sum10 = num4**3
+print(f"{num4}                     {sum9}                {sum10}")
+sum11 = num5**2
+sum12 = num5**3
+print(f"{num5}                     {sum11}                {sum12}")
