b05faabdbdd926dc1b63320a592784c63573223c
HAMID <abdulhamidabari652@gmail.com>
2024-10-19 07:48:31 +0100

    multiple

---
+++ multiple.py
@@ -0,0 +1,21 @@
+num1 = 1024
+num2 = 2
+sum2 = num2 * 2
+sum3 = num1 * 4
+
+if (num1 % sum3)==0:
+	print(f"{num1} is a multiple of 4 ");
+
+if (num1 % sum3)!=0:
+	print(f"{num1} is not  a multiple of 4 ");
+
+if (num2 % sum2)==0:
+	print(f"{num2} is a multiple of 2");
+
+if (num2 % sum2 )!=0:
+	print(f"{num2} is  a multiple of 2");
+
+
+
+
+
