7ed1cb058913aa9d4fcab56c735a65e4eac35063
HAMID <abdulhamidabari652@gmail.com>
2024-10-19 08:19:50 +0100

    space

---
+++ space.py
@@ -0,0 +1,7 @@
+num1 =int(input("enter an integer"))
+num2 =int(input("enter an integer")) 
+num3 =int(input("enter an integer"))
+num4 =int(input("enter an integer"))
+num5 =int(input("enter an integer")) 
+
+print(f"{num2}   {num2}   {num3}   {num4}   {num5}")
