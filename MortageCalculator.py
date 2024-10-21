58d7ff4700d0e6747080af671315b37939da933d
HAMID <abdulhamidabari652@gmail.com>
2024-10-21 21:01:00 +0100

    mortage

--- MortageCalculator.py
+++ MortageCalculator.py
@@ -1,15 +1,15 @@
 print(f"WELCME TO FINTECH COMPANY")
 print(f"where your satisfaction is what we relay on")
 
-lone_amount=int(input("ENTER YOUR DESIRED AMOUNT OF LOAN(#)"))
+loan_amount=int(input("ENTER YOUR DESIRED AMOUNT OF LOAN(#): "))
+interest_rate=int(input("ENTER YOUR interest rate: "))
 
-interest = lone_amount/2
 time_given=(f"YOU ARE GIVEN THE PRIVILEDGE OF 5 MOUNT AFTER THE DAY YOUR LOAN WAS ISSUED")
 
-montly_rate=(interest/110)/12
+duration=int(input("HOW MANY YEARS SHOULD YOU BE GIVEN TO REPAY"))
+montly_rate=(interest_rate/100)/12
+sum_amount=duration*12
 
-montly_mortage=(lone_amount*montly_rate*(1+montly_rate)**2)
+montly_mortage=loan_amount*montly_rate*(1+montly_rate)**sum_amount/ ((1+montly_rate)**sum_amount-1)
 
-print(f"the interest we are going to aquire is: {interest} ")
-print(f"the montly rate is: {montly_rate}")
-print(f"our montly mortage is: {montly_mortage}")
+print(f"enter the duration in year : ${montly_mortage:.2f}")
