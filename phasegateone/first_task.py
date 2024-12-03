efe58f68faa39b2c6cb17e7342d9a1fecbe921c5
HAMID <abdulhamidabari652@gmail.com>
2024-12-03 03:27:56 +0100

    first task update

--- phasegateone/first_task.py
+++ phasegateone/first_task.py
@@ -1,3 +1,24 @@
+"""
+import a class RANDOM
+
+create a variable that gives the  first random number[first_random_number]
+
+create a variable that gives the  second random number[second_random_number]
+
+calculate the total and initialize it to [real_sum]
+
+prompt user to enter his sum 
+collect the [user_input]
+store as [user_input]
+
+if  [real_sum]==[user_input]
+display true
+
+else display false
+"""
+
+
+
 import random
 
 first_random_number = random.randint(1,100)
