1f8bc2f232601db53865e49059e8b42525953d3d
HAMID <abdulhamidabari652@gmail.com>
2024-12-04 04:59:24 +0100

    phase gate

---
+++ phasegate/student_grade_builder.py
@@ -0,0 +1,15 @@
+no_of_student = int(input("How many student do you have: \n"))
+no_of_subject = int(input("How many subject do they offer: \n"))
+score = []
+print("saving >>>>>>>>>>>>>>>> ")
+print("\nsaved successfully \n")
+
+
+for i in range(no_of_student):
+	for j in range(no_of_subject):
+		print(f"Entering score for student: {i}\n Entering score for subject: {j} ");
+		scores = int(input(""))
+		score.append(scores)
+		print("saving >>>>>>>>>>>>>>>> ")
+		print("\nsaved successfully \n")
+		
